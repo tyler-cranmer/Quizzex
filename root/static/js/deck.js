a7c5354477d8ed6b7f74e33104a565e9f6d3ebaa
Benjamin Price <benjamin.price@colorado.edu>
2020-10-16 15:23:47 -0600

    Added Create New Card button to user-library.html

--- root/static/js/deck.js
+++ root/static/js/deck.js
@@ -7,6 +7,7 @@ function save() {
   var name = document.forms[0].elements[0].value;
   var description = document.forms[0].elements[1].value;
   var category = document.forms[0].elements[2].value;
+  // Value of checkbox is only sent when form is submitted.  This variable only says 'on' either way
   var isPublic = document.forms[0].elements[3].value;
 
   alert("Name: " + name + "\nDescription: " + description + "\nCategory: " + category + "\nIs it public?: " + isPublic);
