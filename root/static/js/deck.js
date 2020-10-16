8405f637402574387fe8dfd351cddee576c9e00b
Benjamin Price <benjamin.price@colorado.edu>
2020-10-16 15:19:45 -0600

    Added button functionality to deck.html with deck.js

---
+++ root/static/js/deck.js
@@ -0,0 +1,20 @@
+$('#description').on('input', function() {
+  var numchars = $('#description').val().length;
+  $('#desc-counter').text(numchars + "/300");
+});
+
+function save() {
+  var name = document.forms[0].elements[0].value;
+  var description = document.forms[0].elements[1].value;
+  var category = document.forms[0].elements[2].value;
+  var isPublic = document.forms[0].elements[3].value;
+
+  alert("Name: " + name + "\nDescription: " + description + "\nCategory: " + category + "\nIs it public?: " + isPublic);
+}
+
+function back() {
+  var res = confirm("Your new deck has not been saved.  Are you sure you would like to go back?");
+  if (res) {
+    window.location.href = '/library';
+  }
+}
