47ca686b5946081cb7318a0434391fa760835352
Benjamin Price <benjamin.price@colorado.edu>
2020-11-07 18:44:06 -0700

    Updated signIn function to show alert of form elements to be submitted

--- root/static/js/Signinpage.js
+++ root/static/js/Signinpage.js
@@ -1,3 +1,6 @@
 function signIn() {
+  var username = document.forms[0].elements[0].value;
+  var password = document.forms[0].elements[1].value;
+  alert("This will submit the login for user: " + username + " with password: " + password + ".");
   $("#loginForm").submit();
 }
