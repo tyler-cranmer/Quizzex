2642a817d9274acd9a0854189600dbc926f7ded4
Benjamin Price <benjamin.price@colorado.edu>
2020-11-08 12:02:48 -0700

    Added if else to save() to ensure all fields are filled out

--- root/static/js/deck.js
+++ root/static/js/deck.js
@@ -24,8 +24,13 @@ function save() {
   // Value of checkbox is only sent when form is submitted.  This variable only says 'on' either way
   var isPublic = document.forms[0].elements[3].value;
 
-  alert("Name: " + name + "\nDescription: " + description + "\nCategory: " + category + "\nIs it public?: " + isPublic);
-  document.forms[0].submit();
+  //alert("Name: " + name + "\nDescription: " + description + "\nCategory: " + category + "\nIs it public?: " + isPublic);
+  if(name && description && category) {
+    document.forms[0].submit();
+  } else {
+    alert("Please ensure all fields are completed");
+  }
+
 }
 
 /*
