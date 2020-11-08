1d66cbf0d36efd588086ddc6eccb4149e122bd4f
Benjamin Price <benjamin.price@colorado.edu>
2020-11-08 11:08:07 -0700

    Added documentation to flashcard.py

--- root/flashcard.py
+++ root/flashcard.py
@@ -12,8 +12,9 @@ app = Flask(__name__)
 # REQUIRED for session to work to track username through website
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 
-
-
+###################################
+### Connection for the database ###
+###################################
 mydb = mysql.connector.connect(
     host="35.223.161.145",
     user="root",
@@ -23,6 +24,9 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 mycursor.execute("use flashcards")
 
+########################
+### Helper Functions ###
+########################
 def get_user(username):
     sql = "SELECT * FROM users WHERE username = %s"
     val=(username, )
@@ -44,11 +48,18 @@ def login(username, password):
         else:
             return False
 
+##############
+### ROUTES ###
+##############
 
+# Navigates to langingPage.html, root of the website
 @app.route('/')
 def mysite():
     return render_template('landingPage.html')
 
+# Validates the entered username and password
+# If valid, sets session variables and goes to user's library page
+# TODO: If invalid, returns to login page and display failed message
 @app.route('/doLogin')
 def dologin():
     username=request.args.get('username', None)
@@ -60,24 +71,36 @@ def dologin():
     else:
         return render_template('invalidLogin.html')
 
+# Navigates to create-new-card.html
 @app.route('/createCard')
 def goToCreateCard():
     return render_template('create-new-card.html')
 
+# Adds a new card to the database and returns to user library
+@app.route('/addNewCard')
+def addNewCard():
+    # Function will be run when the save button is pressed and form is submitted for
+    # create-new-card.html page
+    return goToLibrary()
+
+# Navigates to deck.html page
 @app.route('/createDeck')
 def goToCreateDeck():
     return render_template('deck.html')
 
+# Adds a new deck to the database and returns to user library
 @app.route('/addNewDeck')
-def addnewDeck():
+def addNewDeck():
     # Function will be run when the save button is pressed and form is submitted for
     # deck.html page
-    return mysite();
+    return goToLibrary()
 
+# Returns user to their personal library page
 @app.route('/library')
 def goToLibrary():
     return render_template('user-library.html', username=session['username'])
 
+# Navigates to Signinpage.html
 @app.route('/signIn')
 def goToSignIn():
     return render_template('Signinpage.html')
