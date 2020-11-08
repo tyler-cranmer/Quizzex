a63ee59760cc13bbec5aceada584bedff722ff0e
Benjamin Price <benjamin.price@colorado.edu>
2020-11-08 13:29:11 -0700

    removed alert message from save()

--- root/static/js/deck.js
+++ root/static/js/deck.js
@@ -18,19 +18,7 @@ $('#description').on('input', function() {
   - isPublic: sets public option for public/private decks
  */
 function save() {
-  var name = document.forms[0].elements[0].value;
-  var description = document.forms[0].elements[1].value;
-  var category = document.forms[0].elements[2].value;
-  // Value of checkbox is only sent when form is submitted.  This variable only says 'on' either way
-  var isPublic = document.forms[0].elements[3].value;
-
-  //alert("Name: " + name + "\nDescription: " + description + "\nCategory: " + category + "\nIs it public?: " + isPublic);
-  if(name && description && category) {
     document.forms[0].submit();
-  } else {
-    alert("Please ensure all fields are completed");
-  }
-
 }
 
 /*
