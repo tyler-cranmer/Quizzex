a3616d3074c89737d9a56c15e3ff80780a78ce8f
Benjamin Price <benjamin.price@colorado.edu>
2020-11-07 18:46:01 -0700

    Added documentation for signIn function

--- root/static/js/Signinpage.js
+++ root/static/js/Signinpage.js
@@ -1,6 +1,10 @@
+/*
+  signIn will submit the form to the flask file with username and password variables
+  TO DO: return a statement to the user if the credentials are not in the database!!!!
+*/
 function signIn() {
   var username = document.forms[0].elements[0].value;
   var password = document.forms[0].elements[1].value;
   alert("This will submit the login for user: " + username + " with password: " + password + ".");
-  $("#loginForm").submit();
+  //$("#loginForm").submit();
 }
