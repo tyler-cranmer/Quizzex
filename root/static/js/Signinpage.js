eca095fb4b772196bfa2fb96229cf26da039f069
Benjamin Price <benjamin.price@colorado.edu>
2020-11-08 10:09:06 -0700

    Uncommented submit code

--- root/static/js/Signinpage.js
+++ root/static/js/Signinpage.js
@@ -6,5 +6,5 @@ function signIn() {
   var username = document.forms[0].elements[0].value;
   var password = document.forms[0].elements[1].value;
   alert("This will submit the login for user: " + username + " with password: " + password + ".");
-  //$("#loginForm").submit();
+  $("#loginForm").submit();
 }
