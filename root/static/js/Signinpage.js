831230f9d6fb0201ffed6c3a0b7be7cc69f7213c
Benjamin Price <benjamin.price@colorado.edu>
2020-11-08 10:19:17 -0700

    commented out code for troubleshooting

--- root/static/js/Signinpage.js
+++ root/static/js/Signinpage.js
@@ -3,8 +3,8 @@
   TO DO: return a statement to the user if the credentials are not in the database!!!!
 */
 function signIn() {
-  var username = document.forms[0].elements[0].value;
-  var password = document.forms[0].elements[1].value;
-  alert("This will submit the login for user: " + username + " with password: " + password + ".");
+  //var username = document.forms[0].elements[0].value;
+  //var password = document.forms[0].elements[1].value;
+  //alert("This will submit the login for user: " + username + " with password: " + password + ".");
   $("#loginForm").submit();
 }
