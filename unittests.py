8c0e5198381f8accc9dfdb0dc30ca8aee07326a8
allie bernstein <alliebernstein1@gmail.com>
2020-11-09 21:19:28 +0000

    2 user tests implemented

--- unittests.py
+++ unittests.py
@@ -2,6 +2,7 @@ import unittest
 import helper_functions
 from helper_functions import *
 
+
 mydb= connect_db()
 mycursor = mydb.cursor(buffered=True)
 mycursor.execute("use flashcards")
@@ -16,9 +17,16 @@ class TestHelperFunctions(unittest.TestCase):
 
     # Unit Tests below here
 
-    def get_user_info_from_database(self):
-        get_user('Allie') 
-        self.assertEqual((1, 'Allie', 'Password123', 'allie@gmail.com'))
+    def test_get_user_info_from_database(self):
+        self.assertEqual((1,'Allie','Password123','allie@gmail.com'),get_user('Allie'))
+
+    def test_get_user_for_nonexistant_user(self):
+        #this should return None if user is not in database
+        self.assertEqual(None,get_user('Mike'))
+
+    #def testEqual(self):
+    #    self.assertEqual(1, 3-1)
+    
 
 if __name__ == '__main__':
     unittest.main()
