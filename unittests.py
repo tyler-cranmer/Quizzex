c95ad09d634eb0576b68d5a823b9a79cb339fa28
allie bernstein <alliebernstein1@gmail.com>
2020-11-09 23:35:10 +0000

    implemented more unit tests

--- unittests.py
+++ unittests.py
@@ -16,8 +16,7 @@ class TestHelperFunctions(unittest.TestCase):
         pass
 
     # Unit Tests below here
-
-<<<<<<< HEAD
+    
     def test_get_user_info_from_database(self):
         self.assertEqual((1,'Allie','Password123','allie@gmail.com'),get_user('Allie'))
 
@@ -25,14 +24,28 @@ class TestHelperFunctions(unittest.TestCase):
         #this should return None if user is not in database
         self.assertEqual(None,get_user('Mike'))
 
-    #def testEqual(self):
-    #    self.assertEqual(1, 3-1)
+    def test_add_new_user(self):
+        user_id=add_user('Shannon','12345','shannon@gmail.com')
+        #if add user worked, then 'get_user' should return the correct users info
+        self.assertEqual((user_id,'Shannon','12345','shannon@gmail.com'),get_user('Shannon'))
     
-=======
-    def get_user_info_from_database(self):
-        get_user('Allie')
-        self.assertEqual((1, 'Allie', 'Password123', 'allie@gmail.com'))
->>>>>>> 2156cb4cfae211fd568a1f82836a82153f1600fe
+    def test_try_to_add_existing_user(self):
+        user = add_user('Allie','11111', 'allie@gmail.com')
+        #user should not be added if username already exists
+        self.assertEqual(user,"This username is already taken")
+
+    def test_remove_existing_user(self):
+        remove_user('Shannon')
+        #if this works, calling 'get_user' for shannon should return None as they are no longer a user
+        self.assertEqual(None,get_user('Shannon'))
+
+    def test_remove_user_that_does_not_exist(self):
+        fake_user = remove_user('sldkfmsldkfm')
+        #if this works, it should return 'This user does not exist'
+        self.assertEqual(fake_user, 'This user does not exist')
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
