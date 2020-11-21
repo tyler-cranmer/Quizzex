0d5ca43d92f2770fd8cddd02bae5145f43c2e1e5
Sam Terry <samuelbuckleyterry@gmail.com>
2020-11-21 06:24:49 -0600

    Added the 'run.py' script to the main directory. This is a shortcut; if you're in the projectRegex directory, you can start the server by running 'python run.py' from the command line.

---
+++ run.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.call("set FLASK_APP=root/flashcard.py", shell=True)
+subprocess.call("export FLASK_APP=root/flashcard.py", shell=True)
+subprocess.call('flask run', shell=True)
