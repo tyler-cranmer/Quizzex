1a8a8a7703425081ff08a0acee704009dfeb0963
Benjamin Price <benjamin.price@colorado.edu>
2020-11-21 14:31:30 -0700

    Added check for if user_decks is empty

--- root/flashcard.py
+++ root/flashcard.py
@@ -147,14 +147,16 @@ def goToLibrary():
     user = session['username']
     # retrieve a list of decks from database in the form [("deckName1",),("deckName2",),...]
     user_decks = get_decks(user)
-    deck_html = ""
     deck_count = 0
     # For each deck, increment count and create an html form with delete and open buttons
-    for deck in user_decks:
-        deck_html = (deck_html + '<form action="/doDeckOpenOrDelete"><h2 class="deck">' + deck[0] + '</h2>'+
-            '<input name="open" class="deck_button" type="submit" value="Open">' +
-            '<input name="delete" class="deck_button" type="submit" value="Delete"></form>')
-        deck_count = deck_count + 1
+    if(user_decks):
+        for deck in user_decks:
+            deck_html = (deck_html + '<form action="/doDeckOpenOrDelete"><h2 class="deck">' + deck[0] + '</h2>'+
+                '<input name="open" class="deck_button" type="submit" value="Open">' +
+                '<input name="delete" class="deck_button" type="submit" value="Delete"></form>')
+            deck_count = deck_count + 1
+    else:
+        user_decks = None
     return render_template('user-library.html', username=user, decks = deck_html, numdecks = deck_count)
 
 # Navigates to Signinpage.html
