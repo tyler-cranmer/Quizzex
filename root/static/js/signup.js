bf67063a5589f9f0fcdd963a9a783ab54a8395ca
Sam Terry <samuelbuckleyterry@gmail.com>
2020-11-21 05:09:47 -0600

    Created a signup.js file with a signUp function This function submits the new login info to the '/doSignup' route in the flashcard.py Flask file

---
+++ root/static/js/signup.js
@@ -0,0 +1,7 @@
+/*
+    signUp will get the form with the username, email, password, & confirm password variables
+    It will then send this form to the Flask file
+*/
+function signUp() {
+    $("#SignupForm").submit();
+}
