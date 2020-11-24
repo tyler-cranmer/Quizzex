61374f9e2ceea2103959d76eb54dc2ce9e9dfa65
Benjamin Price <benjamin.price@colorado.edu>
2020-11-24 16:07:12 -0700

    missing > typo fix

--- root/flashcard.py
+++ root/flashcard.py
@@ -176,11 +176,11 @@ def goToLibrary():
                 '<input name="deckname" type="hidden" value="' + deck[0] + '"/>' +
                 '<input name="study" class="deck" type="submit" value="' + deck[0] + '"/>' +
                 '</form>' +
-                '<form method="POST" action="/doDeckFunction" class=""' +
+                '<form method="POST" action="/doDeckFunction" class="">' +
                 '<input name="deckname" type="hidden" value="' + deck[0] + '"/>' +
                 '<input name="edit" class="deck_button_img" type="image" src="static/img/editIcon.jpg" alt="Edit"/>' +
                 '</form>' +
-                '<form method="POST" action="/doDeckFunction" class=""' +
+                '<form method="POST" action="/doDeckFunction" class="">' +
                 '<input name="deckname" type="hidden" value="' + deck[0] + '"/>' +
                 '<img name="delete" class="delete_btn deck_button_img" src="static/img/trashcan.png" alt="Delete">' +
                 '</form><br>' +
